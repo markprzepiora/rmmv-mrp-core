93cc715b485abfda4d6fd305cee8a65b65b2e61b
Mark Przepiora <mark.przepiora@gmail.com>
2015-10-31 15:47:54 -0600

    Refactor LexerUtils to include token string positions

--- src/OptionParser/LexerUtils.js
+++ src/OptionParser/LexerUtils.js
@@ -1,13 +1,46 @@
+// type: e.g. 'UNDERSCORE'
+// token: e.g. '_'
+// pos: the (starting) position in the string where it occurred
+function Token(type, token, pos) {
+  return { type, token, pos };
+}
+
+// tokens: an array of tokens
+// newCharacterStream: a new character stream for the next lexer
+function LexerResponse(tokens, newCharacterStream) {
+  return { tokens, newCharacterStream };
+}
+
+// fullString: the underlying buffer
+// pos: the 'zero' index of the stream
+//
+// str() returns the slice from position pos
+// from(i) returns a new character stream starting at pos + i
+//
+// The calling code can pretend they're just dealing with the slice, but we
+// keep track of where we are in the underlying string.
+function CharacterStream(fullString, pos = 0) {
+  return {
+    fullString,
+    pos,
+    empty: pos >= fullString.length,
+    str: () => fullString.slice(pos),
+    from: (index) => CharacterStream(fullString, pos + index),
+    Token: (type, token) => Token(type, token, pos),
+    flush: () => CharacterStream(fullString, fullString.length)
+  }
+}
+
 export function regex(type, regex, flags = '') {
   var massagedRegex = new RegExp(/^/.source + regex.source, flags);
 
-  return function(previousTokens, str) {
+  return function(previousTokens, charStream) {
     var match;
-    if (match = str.match(massagedRegex)) {
-      return [
-        [{ type: type, token: match[0] }],
-        str.slice(match[0].length)
-      ];
+    if (match = charStream.str().match(massagedRegex)) {
+      return LexerResponse(
+        [charStream.Token(type, match[0])],
+        charStream.from(match[0].length)
+      );
     } else {
       return null;
     }
@@ -16,11 +49,10 @@ export function regex(type, regex, flags = '') {
 
 // Like the regex matcher, but throws away the matched token.
 export function skip(baseMatcher) {
-  return function(previousTokens, str) {
+  return function(previousTokens, charStream) {
     var match;
-    if (match = baseMatcher(previousTokens, str)) {
-      const [tokens, nextStr] = match;
-      return [[], nextStr];
+    if (match = baseMatcher(previousTokens, charStream)) {
+      return LexerResponse([], match.newCharacterStream);
     } else {
       return null;
     }
@@ -28,39 +60,35 @@ export function skip(baseMatcher) {
 }
 
 export function optional(matcher) {
-  return function(previousTokens, str) {
+  return function(previousTokens, charStream) {
     var match;
-    if (match = matcher(previousTokens, str)) {
-      const [tokens, nextStr] = match;
-      return [[], nextStr];
+    if (match = matcher(previousTokens, charStream)) {
+      return LexerResponse([], match.newCharacterStream);
     } else {
-      return [[], str];
+      return LexerResponse([], charStream);
     }
   }
 }
 
 export function seq2(first, second) {
-  return function(previousTokens, str) {
-    const firstMatch = first(previousTokens, str);
+  return function(previousTokens, charStream) {
+    const firstMatch = first(previousTokens, charStream);
 
     if (!firstMatch) {
       return null;
     }
 
-    const [firstTokens, nextStr] = firstMatch;
     const secondMatch =
-      second([...previousTokens, ...firstTokens], nextStr);
+      second([...previousTokens, ...firstMatch.tokens], firstMatch.newCharacterStream);
 
     if (!secondMatch) {
       return null;
     }
 
-    const [secondTokens, outputNextStr] = secondMatch;
-
-    return [
-      [...firstTokens, ...secondTokens],
-      outputNextStr
-    ];
+    return LexerResponse(
+      [...firstMatch.tokens, ...secondMatch.tokens],
+      secondMatch.newCharacterStream
+    );
   }
 }
 
@@ -75,10 +103,10 @@ export function seq(firstMatcher, secondMatcher, thirdMatcher, ...rest) {
 }
 
 export function precededByToken(type) {
-  return function(previousTokens, str) {
+  return function(previousTokens, charStream) {
     const lastToken = previousTokens[previousTokens.length - 1];
     if (lastToken && lastToken.type == type) {
-      return [[], str];
+      return LexerResponse([], charStream);
     } else {
       return null;
     }
@@ -86,14 +114,14 @@ export function precededByToken(type) {
 }
 
 export function map(fn, matcher) {
-  return function(previousTokens, str) {
+  return function(previousTokens, charStream) {
     var match;
-    if (match = matcher(previousTokens, str)) {
-      const [newTokens, newStr] = match;
-      const mappedTokens = newTokens.map(
-        ({ type, token }) => ({ type: type, token: fn(token) }));
+    if (match = matcher(previousTokens, charStream)) {
+      const mappedTokens = match.tokens.map(
+        ({ type, token, pos }) => Token(type, fn(token), pos));
 
-      return [mappedTokens, newStr];
+      return LexerResponse(
+        mappedTokens, match.newCharacterStream);
     } else {
       return null;
     }
@@ -101,10 +129,10 @@ export function map(fn, matcher) {
 }
 
 export function or(...matchers) {
-  return function(previousTokens, str) {
+  return function(previousTokens, charStream) {
     var match;
     for (var i = 0; i < matchers.length; i++) {
-      if (match = matchers[i](previousTokens, str)) {
+      if (match = matchers[i](previousTokens, charStream)) {
         return match;
       }
     }
@@ -119,15 +147,23 @@ export function Lexer(_lexer, str) {
     var newTokens, newStr;
     var counter = 0;
 
-    while (str.length > 0) {
-      [newTokens, str] = _lexer(tokens, str) || [
-        [{ type: 'UNKNOWN', token: str }], ""
-      ];
-      tokens = [...tokens, ...newTokens];
+    var charStream = CharacterStream(str);
+
+    while (!charStream.empty) {
+      var match =
+        _lexer(tokens, charStream) ||
+        LexerResponse(
+          [charStream.Token('UNKNOWN', charStream.str())],
+          charStream.str().length
+        );
+
+      tokens = [...tokens, ...match.tokens];
 
       if (counter++ > 10000) {
         throw "tried to lex more than 10,000 tokens - this is probably a bug.";
       }
+
+      charStream = match.newCharacterStream;
     }
 
     return tokens;
