8b00f683f48136f7b0123b765db50177285ff4b4
Mark Przepiora <mark.przepiora@gmail.com>
2015-11-02 23:17:57 -0700

    Add build script

---
+++ script/build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# This script builds an ES5 npm package inside the `build` directory, ready to
+# be pushed to npm.
+
+set -eu
+set -o pipefail
+set +x
+
+readonly ARGS="$@"
+readonly LONGPROGNAME=$(perl -m'Cwd' -e 'print Cwd::abs_path(@ARGV[0])' "$0")
+readonly PROGDIR="${LONGPROGNAME%/*}"     # get directory component (remove short match)
+readonly PROGNAME="${LONGPROGNAME##*/}"   # get basename component (remove long match)
+
+set -x
+
+cd "$PROGDIR"/..
+
+rm -rf build
+mkdir build
+cp package.json README.md build/
+node_modules/babel-cli/bin/babel.js src/module --out-dir build
